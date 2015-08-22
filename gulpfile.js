22ae34838745efe73e693499edb85eb210a2fdf5
Tony Spiro <tspiro@tonyspiro.com>
2015-08-22 13:29:24 -0500

    bundle.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
  
 gulp.task('build', function () {
-  browserify({
+  return browserify({
     entries: 'app.js',
     extensions: ['.jsx'],
     debug: true
@@ -15,8 +18,17 @@ gulp.task('build', function () {
   .pipe(source('bundle.js'))
   .pipe(gulp.dest('dist'));
 });
- 
-gulp.task('default', ['build']);
+
+gulp.task('compress', function() {
+  return gulp.src('./dist/bundle.js')
+  	.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+	  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function(cb) {
+  runSequence('build','compress', cb);
+});
 
 gulp.task('watch', function () {
  	gulp.watch("./*.js", ['default']);
